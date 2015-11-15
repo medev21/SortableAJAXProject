bddefc214e534c406bf8c308ca7cc13518fbb516
Martin Ben <mebenav88@gmail.com>
2015-11-14 22:58:14 -0500

    add new create controller and templates

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,24 @@
 class TasksController < ApplicationController
   def index
-  end 
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+
+    if @task.save
+      redirect_to root_path, notice: 'Task save succesfully'
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:title, :description)
+    end
 end
